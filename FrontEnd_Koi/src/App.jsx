387e3b99519e88a878fd89d8e5a18a1d06b58edd
Qui Nguyen <quinguyen.2924@gmail.com>
2024-10-01 17:55:00 +0700

    fix_css

--- FrontEnd_Koi/src/App.jsx
+++ FrontEnd_Koi/src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
-import './App.css'
-import Admin from './pages/admin/Admin';
-import Dashboard from "./pages/admin/Dashboard.jsx";
-
+import Header from "./components/header.jsx";
+import Footer from "./components/footer.jsx";
+import TrangChu from "./pages/Trangchu/Trangchu.jsx";
 
 function App() {
   return (
@@ -15,16 +12,21 @@ function App() {
 
 
 
-            <Routes>
-                    <Route path="/"  element={<Homepages />} />
-                    <Route path="/admin" element={<Admin />} />
-                    <Route path="Dashboard" element={<Dashboard />} />
-                    <Route path="ProjectManage" element={<ProjectManage />} />
-                    <Route path="SampleProjects" element={<SampleProjects />} />
-                    <Route path="ServiceManage" element={<ServiceManage />} />
-                    <Route path="UserManage" element={<UserManage />} />
+            {/*<Routes>*/}
+            {/*        <Route path="/"  element={<Homepages />} />*/}
+            {/*        <Route path="/admin" element={<Admin />} />*/}
+            {/*        <Route path="Dashboard" element={<Dashboard />} />*/}
+            {/*        <Route path="ProjectManage" element={<ProjectManage />} />*/}
+            {/*        <Route path="SampleProjects" element={<SampleProjects />} />*/}
+            {/*        <Route path="ServiceManage" element={<ServiceManage />} />*/}
+            {/*        <Route path="UserManage" element={<UserManage />} />*/}
 
-            </Routes>
+            {/*</Routes>*/}
+            <Header/>
+            <TrangChu/>
+            <Footer/>
+
+
         </div>
   )
 }
