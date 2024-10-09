252ca86b0de818e36a5427b1db1416207ecd1d9b
Qui Nguyen <quinguyen.2924@gmail.com>
2024-10-09 20:41:38 +0700

    projects/project-item
    done

--- FrontEnd_Koi/src/pages/Duan/ProjectPage.jsx
+++ FrontEnd_Koi/src/pages/Duan/ProjectPage.jsx
@@ -1,13 +1,11 @@
 import Header from '../../components/header';
 import Footer from '../../components/footer';
 import ProjectList from "./ProjectList.jsx";
-import Banner from "./banner";
 const ProjectPage = () => {
     return (
         <div>
 
             <Header/>
-            <Banner/>
             <ProjectList/>
 
             <Footer/>
