c39940c0ff4fe10bb87199542c63fd3f588b27bb
DucThinh <2251120054@ut.edu.vn>
2024-10-12 00:26:14 +0700

    add file Card_data.jsx

---
+++ FrontEnd_Koi/src/pages/Blog/Card_data.jsx
@@ -0,0 +1,92 @@
+import "./Blog.css";
+import Y_tuong_nha_choi_la_dep from "../../assets/image/y-tuong-nha-choi-la-dep.jpg";
+
+const cardData = [
+  {
+    image: Y_tuong_nha_choi_la_dep,
+    title: "Những ý tưởng nhà chòi lá tuyệt đẹp cho sân vườn",
+    description: "Nhà chòi lá là dạng cấu trúc đơn giản, nhỏ gọn, thường...",
+    date: "11/10/2024",
+    url: "https://example.com/1",
+  },
+  {
+    image: Y_tuong_nha_choi_la_dep,
+    title: "33+ mẫu nhà gỗ sân vườn đẹp ấn tượng nhất 2024",
+    description:
+      "Nhà gỗ sân vườn, chòi nghỉ sân vườn hoặc nhà chòi gỗ sân vườn...",
+    date: "10/10/2024",
+    url: "https://example.com/1",
+  },
+  {
+    image: Y_tuong_nha_choi_la_dep,
+    title: "99+ mẫu nhà chòi tứ giác, lục giác bằng sắt đẹp độc đáo",
+    description: "Ở bài viết trước SGL Vietnam đã gửi đến bạn đọc những mẫu...",
+    date: "09/10/2024",
+    url: "https://example.com/1",
+  },
+  {
+    image: Y_tuong_nha_choi_la_dep,
+    title: "99+ mẫu nhà chòi tứ giác, lục giác bằng sắt đẹp độc đáo",
+    description: "Ở bài viết trước SGL Vietnam đã gửi đến bạn đọc những mẫu...",
+    date: "09/10/2024",
+    url: "https://example.com/1",
+  },
+  {
+    image: Y_tuong_nha_choi_la_dep,
+    title: "99+ mẫu nhà chòi tứ giác, lục giác bằng sắt đẹp độc đáo",
+    description: "Ở bài viết trước SGL Vietnam đã gửi đến bạn đọc những mẫu...",
+    date: "09/10/2024",
+    url: "https://example.com/1",
+  },
+  {
+    image: Y_tuong_nha_choi_la_dep,
+    title: "99+ mẫu nhà chòi tứ giác, lục giác bằng sắt đẹp độc đáo",
+    description: "Ở bài viết trước SGL Vietnam đã gửi đến bạn đọc những mẫu...",
+    date: "09/10/2024",
+    url: "https://example.com/1",
+  },
+  {
+    image: Y_tuong_nha_choi_la_dep,
+    title: "99+ mẫu nhà chòi tứ giác, lục giác bằng sắt đẹp độc đáo",
+    description: "Ở bài viết trước SGL Vietnam đã gửi đến bạn đọc những mẫu...",
+    date: "09/10/2024",
+    url: "https://example.com/1",
+  },
+  {
+    image: Y_tuong_nha_choi_la_dep,
+    title: "99+ mẫu nhà chòi tứ giác, lục giác bằng sắt đẹp độc đáo",
+    description: "Ở bài viết trước SGL Vietnam đã gửi đến bạn đọc những mẫu...",
+    date: "09/10/2024",
+    url: "https://example.com/1",
+  },
+  {
+    image: Y_tuong_nha_choi_la_dep,
+    title: "99+ mẫu nhà chòi tứ giác, lục giác bằng sắt đẹp độc đáo",
+    description: "Ở bài viết trước SGL Vietnam đã gửi đến bạn đọc những mẫu...",
+    date: "09/10/2024",
+    url: "https://example.com/1",
+  },
+  {
+    image: Y_tuong_nha_choi_la_dep,
+    title: "99+ mẫu nhà chòi tứ giác, lục giác bằng sắt đẹp độc đáo",
+    description: "Ở bài viết trước SGL Vietnam đã gửi đến bạn đọc những mẫu...",
+    date: "09/10/2024",
+    url: "https://example.com/1",
+  },
+  {
+    image: Y_tuong_nha_choi_la_dep,
+    title: "99+ mẫu nhà chòi tứ giác, lục giác bằng sắt đẹp độc đáo",
+    description: "Ở bài viết trước SGL Vietnam đã gửi đến bạn đọc những mẫu...",
+    date: "09/10/2024",
+    url: "https://example.com/1",
+  },
+  {
+    image: Y_tuong_nha_choi_la_dep,
+    title: "99+ mẫu nhà chòi tứ giác, lục giác bằng sắt đẹp độc đáo",
+    description: "Ở bài viết trước SGL Vietnam đã gửi đến bạn đọc những mẫu...",
+    date: "09/10/2024",
+    url: "https://example.com/1",
+  },
+];
+
+export default cardData;
