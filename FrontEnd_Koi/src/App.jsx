40f7878f0f9e1a1f13deb269383c53f5275ca15a
DucThinh <2251120054@ut.edu.vn>
2024-10-24 21:09:50 +0700

    Blog-update-1

--- FrontEnd_Koi/src/App.jsx
+++ FrontEnd_Koi/src/App.jsx
@@ -4,22 +4,28 @@ import Dashboard from "./pages/admin/Dashboard.jsx";
 import Trangchu from "./pages/Trangchu/Trangchu.jsx";
 import UserManage from "./pages/admin/UserManage.jsx";
 import OrdersManage from "./pages/admin/OrdersManage.jsx";
-import ProjectPage from "./pages/Duan/ProjectPage.jsx";
-import ProjectDetail from "./pages/Duan/ProjectDetail.jsx";
 import AdminRoute from "./components/admin/AdminRoute.jsx";
 import ProjectManage from "./pages/admin/ProjectManage.jsx";
 import UserRoute from "./components/UserRoute.jsx";
+import ProjectPage from "./pages/Duan/ProjectPage.jsx";
+import ProjectDetail from "./pages/Duan/ProjectDetail.jsx";
+import BlogPage from "./pages/Blog/Blog_page.jsx";
+import BlogDetail from "./pages/Blog/Blog_detail.jsx";
+import BlogCategory from "./pages/Blog/Blog_category.jsx";
 
 function App() {
     return (
         <div className="App">
             <Router>
                 <Routes>
-                    {/*Phần route dành cho user*/}
                     <Route path="/" element={<UserRoute element={Trangchu} />} />
                     <Route path="/Home" element={<UserRoute element={Trangchu} />} />
                     <Route path="/Project" element={<UserRoute element={ProjectPage} />}/>
                     <Route path="/project/:projectName" element={<UserRoute element={ProjectDetail} />} />
+
+                    <Route path="/Blog" element={<UserRoute element={BlogPage}/>} />
+                    <Route path="/blog/:blogName" element={<UserRoute element={BlogDetail} />}/>
+                    <Route path="/category/:categoryName" element={<UserRoute element={BlogCategory} />} />
 
                     {/*Phần route dành cho admin*/}
                     <Route path="/Admin/dashboard" element={<AdminRoute element={Dashboard} />} />
