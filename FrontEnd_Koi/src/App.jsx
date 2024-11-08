b75b3d8ddfd519967d750a943a25e00b199e30fe
thanh vinh <vinhthanhnguyen12122004@gmail.com>
2024-11-08 18:12:00 +0700

    sửa app.jsx

--- FrontEnd_Koi/src/App.jsx
+++ FrontEnd_Koi/src/App.jsx
@@ -1,7 +1,7 @@
-import {Routes, Route} from 'react-router-dom';
-import {ToastContainer} from 'react-toastify';
-import {useState} from 'react';
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { useState } from "react";
+import "./App.css";
 import Dashboard from "./pages/admin/Dashboard.jsx";
 import Trangchu from "./pages/Trangchu/Trangchu.jsx";
 import UserManage from "./pages/admin/UserManage.jsx";
@@ -18,75 +18,108 @@ import BlogCategory from "./pages/Blog/Blog_category.jsx";
 import Dichvu from "./pages/Dichvu/Dichvu.jsx";
 import DichvuDetail from "./pages/Dichvu/DichvuDetail.jsx";
 import OrderForm from "./pages/Dathang/OrderForm.jsx";
-import {useEffect} from "react";
-import {jwtDecode} from "jwt-decode";
-import {useNavigate} from "react-router-dom";
+import { useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 import GioiThieu from "./pages/gioithieu/gioithieu.jsx";
 import DesignStaffPage from "./pages/nhanvienthietke/DesignStaffPage.jsx";
 import Csbm from "./pages/csbm/csbm.jsx";
 
 function App() {
-    const navigate = useNavigate();
-    const [isFirstLoad, setIsFirstLoad] = useState(true);
-    useEffect(() => {
-        if (isFirstLoad) {
-            const token = localStorage.getItem('token');
-            if (token) {
-                try {
-                    const decoded = jwtDecode(token);
-                    const userRole = decoded.role;
-                    const currentPath = window.location.pathname;
+  const navigate = useNavigate();
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
+  useEffect(() => {
+    if (isFirstLoad) {
+      const token = localStorage.getItem("token");
+      if (token) {
+        try {
+          const decoded = jwtDecode(token);
+          const userRole = decoded.role;
+          const currentPath = window.location.pathname;
 
-                    if (userRole === 'ROLE_MANAGER' && currentPath !== '/Admin/dashboard') {
-                        navigate('/Admin/dashboard');
-                    } else if (userRole === 'ROLE_USER' && currentPath !== '/Home') {
-                        navigate('/Home');
-                    }
-                } catch (error) {
-                    console.error("Token decoding failed:", error);
-                }
-            } else {
-                navigate('/Home');
-            }
-            setIsFirstLoad(false);
+          if (
+            userRole === "ROLE_MANAGER" &&
+            currentPath !== "/Admin/dashboard"
+          ) {
+            navigate("/Admin/dashboard");
+          } else if (userRole === "ROLE_USER" && currentPath !== "/Home") {
+            navigate("/Home");
+          }
+        } catch (error) {
+          console.error("Token decoding failed:", error);
         }
-    }, [navigate, isFirstLoad]);
-        return (
-            <div className="App">
-                <ToastContainer/>
-
-                    <Routes>
-                        {/*Phần route dành cho user*/}
-                        <Route path="/" element={<UserRoute element={<Trangchu/>}/>}/>
-                        <Route path="/Home" element={<UserRoute element={<Trangchu/>}/>}/>
-                        <Route path="/Project" element={<UserRoute element={<ProjectPage/>}/>}/>
-                        <Route path="/project/:projectName" element={<UserRoute element={<ProjectDetail/>}/>}/>
-                        <Route
-                            path="/GioiThieu"
-                            element={<UserRoute element={<GioiThieu/>} />}
-                        />
-                        <Route path="/nhanvien" element={<DesignStaffPage />} />
-                        <Route path="/csbm" element={<UserRoute element={<Csbm/>} />} />
-
-                        <Route path={"/VerifyOTP"} element={<VerifyOTP/>}/>
-
-                        <Route path="/Blog" element={<UserRoute element={<BlogPage/>}/>}/>
-                        <Route path="/blog/:blogName" element={<UserRoute element={<BlogDetail/>}/>}/>
-                        <Route path="/category/:categoryName" element={<UserRoute element={<BlogCategory/>}/>}/>
-
-                        <Route path="/Dichvu" element={<UserRoute element={<Dichvu/>}/>}/>
-                        <Route path="/dichvu/:id" element={<UserRoute element={<DichvuDetail/>}/>}/>
-
-                        <Route path="/OrderForm" element={<UserRoute element={<OrderForm/>}/>}/>
-
-                        {/*Phần route dành cho admin*/}
-                        <Route path="/Admin/dashboard" element={<AdminRoute element={<Dashboard />}/>}/>
-                        <Route path="/Admin/user" element={<AdminRoute element={ <UserManage />}/>}/>
-                        <Route path="/Admin/orders" element={<AdminRoute element={<OrdersManage/>}/>}/>
-                        <Route path="/Admin/ProjectManage" element={<AdminRoute element={<ProjectManage/>}/>}/>
-                    </Routes>
-            </div>
-        )
+      } else {
+        navigate("/Home");
+      }
+      setIsFirstLoad(false);
     }
+  }, [navigate, isFirstLoad]);
+  return (
+    <div className="App">
+      <ToastContainer />
+
+      <Routes>
+        {/*Phần route dành cho user*/}
+        <Route path="/" element={<UserRoute element={<Trangchu />} />} />
+        <Route path="/Home" element={<UserRoute element={<Trangchu />} />} />
+        <Route
+          path="/Project"
+          element={<UserRoute element={<ProjectPage />} />}
+        />
+        <Route
+          path="/project/:projectName"
+          element={<UserRoute element={<ProjectDetail />} />}
+        />
+        <Route
+          path="/GioiThieu"
+          element={<UserRoute element={<GioiThieu />} />}
+        />
+        <Route path="/nhanvien" element={<DesignStaffPage />} />
+        <Route path="/csbm" element={<UserRoute element={<Csbm />} />} />
+
+        <Route path={"/VerifyOTP"} element={<VerifyOTP />} />
+
+        <Route path="/Blog" element={<UserRoute element={<BlogPage />} />} />
+        <Route
+          path="/blog/:blogName"
+          element={<UserRoute element={<BlogDetail />} />}
+        />
+        <Route
+          path="/category/:categoryName"
+          element={<UserRoute element={<BlogCategory />} />}
+        />
+
+        <Route path="/Dichvu" element={<UserRoute element={<Dichvu />} />} />
+        <Route
+          path="/dichvu/:id"
+          element={<UserRoute element={<DichvuDetail />} />}
+        />
+
+        <Route
+          path="/OrderForm"
+          element={<UserRoute element={<OrderForm />} />}
+        />
+
+        {/*Phần route dành cho admin*/}
+        <Route
+          path="/Admin/dashboard"
+          element={<AdminRoute element={<Dashboard />} />}
+        />
+        <Route
+          path="/Admin/user"
+          element={<AdminRoute element={<UserManage />} />}
+        />
+        <Route
+          path="/Admin/orders"
+          element={<AdminRoute element={<OrdersManage />} />}
+        />
+        <Route
+          path="/Admin/ProjectManage"
+          element={<AdminRoute element={<ProjectManage />} />}
+        />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
